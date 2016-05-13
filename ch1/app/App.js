4015e2228dd3786be6fdc2f051906cf365c5c909
sartre-mauro <sartre_omar@hotmail.com>
2016-05-13 10:11:41 +0200

    feature: new version. 	Added Components

--- ch1/app/App.js
+++ ch1/app/App.js
@@ -1,30 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var App = React.createClass({
+var RecentChangesTable = React.createClass({
   render: function() {
-    var headings = this.props.headings.map(function (heading) {
-      return (<th>
-        {heading}
-      </th>);
-    });
+    return (
+      <table className="recent-changes-table">
+        {this.props.children}
+      </table>
+    );
+  }
+});
 
-    var rows = this.props.data.map(function (row) {
-      return (<tr>
-        <td>{row.when}</td>
-        <td>{row.who}</td>
-        <td>{row.desc}</td>
-      </tr>);
+RecentChangesTable.Heading = React.createClass({
+  render: function() {
+    return (
+      <th>{this.props.heading}</th>
+    );
+  }
+});
+
+RecentChangesTable.Headings = React.createClass({
+  render: function() {
+    var headings = this.props.headings.map(function (name, index) {
+      return <RecentChangesTable.Heading
+        key = {index}
+        heading = {name} />;
+    });
+    return (
+      <thead><tr>{headings}</tr></thead>
+    );
+  }
+});
+
+RecentChangesTable.Row = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.changeSet.when}</td>
+        <td>{this.props.changeSet.who}</td>
+        <td>{this.props.changeSet.desc}</td>
+      </tr>
+    );
+  }
+});
+
+RecentChangesTable.Rows = React.createClass({
+  render: function() {
+    var rows = this.props.changeSets.map(function (changeSet, index) {
+      return <RecentChangesTable.Row
+        changeSet = {changeSet}
+        key = {index} />;
     });
 
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <table>
-          <thead>{headings}</thead>
-          {rows}
-        </table>
-      </div>
+      <tbody>{rows}</tbody>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <RecentChangesTable>
+        <RecentChangesTable.Headings headings = {this.props.headings} />
+        <RecentChangesTable.Rows changeSets = {this.props.changeSets} />
+      </RecentChangesTable>
     );
   }
 });
@@ -47,10 +87,13 @@ var data = [
   },
 ];
 
-var headings = ["Last updated at", "By Author", "Summary"];
+// var headings = ["Last updated at", "By Author", "Summary"];
+var headings = ["Updated at", "Author", "Change"];
 var title = "Recent Changes";
 
+var props = { headings: headings, changeSets: data, title: title };
+
 ReactDOM.render(
-  <App headings={headings} data={data} title={title} />,
-  document.body
+  <App {...props} />,
+  document.getElementById("container")
 );
