e9c718b9b992d929cfa0a12279b4f61f2dcf8db0
sartre-mauro <sartre_omar@hotmail.com>
2016-05-16 22:55:51 +0200

    feature: add the shippment track

--- ch4/src/BookStore.js
+++ ch4/src/BookStore.js
@@ -93,19 +93,157 @@ var BookList = React.createClass({
 });
 
 var ShippingDetails = React.createClass({
+  getInitialState() {
+    return ({
+      fullName: "", contactNumber: "", shippingAddress: "", error: false
+    });
+  },
+
+  _renderError() {
+    if (this.state.error) {
+      return (
+        <div className="alert alert-danger">
+          {this.state.error}
+        </div>
+      );
+    }
+  },
+
+  _validateInput() {
+    if (this.state.fullName === '') {
+      this.setState({
+        error: "Please enter full name"
+      });
+    } else if (this.state.contactNumber === '') {
+      this.setState({
+        error: "Please enter contact number"
+      });
+    } else if (this.state.shippingAddress === '') {
+      this.setState({
+        error: "Please enter shipping address"
+      });
+    } else {
+      this.setState({ error: false });
+      return true;
+    }
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    var formData = {
+      fullName: this.state.fullName,
+      contactNumber: this.state.contactNumber,
+      shippingAddress: this.state.shippingAddress
+    };
+
+    if (this._validateInput()) {
+      this.props.updateFormData(formData);
+    }
+  },
+
+  handleChange(event, attribute) {
+    var newState = this.state;
+    newState[attribute] = event.target.value;
+    this.setState(newState);
+    console.log(this.state);
+  },
+
   render() {
+    var errorMessage = this._renderError();
+
     return (
-      <h1>Enter your shipping information.</h1>
-  );
-}
+      <div>
+        <h1>Enter your shipping information.</h1>
+        {errorMessage}
+        <div style={{width: 200}}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input className="form-control"
+                type="text"
+                placeholder="Full Name"
+                value={this.state.fullName}
+                onChange={(event) => this.handleChange(event, "fullName")}
+                />
+            </div>
+            <div className="form-group">
+              <input className="form-control"
+                type="text"
+                placeholder="Contact Number"
+                value={this.state.contactNumber}
+                onChange={(event) => this.handleChange(event, "contactNumber")}
+                />
+            </div>
+            <div className="form-group">
+              <input className="form-control"
+                type="text"
+                placeholder="Shipping Address"
+                value={this.state.shippingAddress}
+                onChange={(event) => this.handleChange(event, "shippingAddress")}
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit"
+                ref="submit"
+                className="btn btn-success">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 });
 
 var DeliveryDetails = React.createClass({
+  getInitialState() {
+    return ({ deliveryOption: "Primary" });
+  },
+
+  handleChange(event) {
+    this.setState({
+      deliveryOption:  event.target.value
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.updateFormData(this.state);
+  },
+
   render() {
     return (
-      <h1>Choose your delivery options here.</h1>
-  );
-}
+      <div>
+        <h1>Choose your delivery options here.</h1>
+        <div style={{width: 200}}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="radio">
+              <label>
+                <input type="radio"
+                  checked={this.state.deliveryOption === "Primary"}
+                  value="Primary"
+                  onChange={this.handleChange}
+                />Primary -- Next day delivery
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input type="radio"
+                  checked={this.state.deliveryOption === "Normal"}
+                  value="Normal"
+                  onChange={this.handleChange}
+                />Normal -- 3-4 days
+              </label>
+            </div>
+            <button className="btn btn-success">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 });
 
 var BookStore = React.createClass({
